fb8a4c7861b7b961cb77d2cbb2f083a79e1b9c4b
Eric <ericnguyen23@gmail.com>
2023-04-25 06:30:18 -0700

    Added react icon set

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import { FaGithub, FaLinkedin, FaChevronUp } from "react-icons/fa";
 
 function Footer(props) {
   return (
     <Container fluid className="p-5" id="footer">
       <p>
-        Eric Nguyen | <a href="https://github.com/ericnguyen23">Github</a> |{" "}
-        <a href="https://www.linkedin.com/in/ericnguyen23/">LinkedIn</a>
+        Eric Nguyen |{" "}
+        <a href="https://github.com/ericnguyen23">
+          <FaGithub />
+        </a>{" "}
+        |{" "}
+        <a href="https://www.linkedin.com/in/ericnguyen23/">
+          <FaLinkedin />
+        </a>
       </p>
       <p>
         <a href="#top" onClick={props.removeActive}>
-          ^ Top
+          <FaChevronUp />
         </a>
       </p>
     </Container>
